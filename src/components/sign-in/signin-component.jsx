916abd8c62397bcb312738d9d7d2910d52c8496c
Lourent Flores <lourent.flores@gmail.com>
2021-01-28 21:17:14 -0500

    completeted signin/signup auth

--- src/components/sign-in/signin-component.jsx
+++ src/components/sign-in/signin-component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input-component';
 import CustomButton from '../custom-button/custom-button-component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
 import './signin-style.css';
 
 class SignIn extends React.Component {
@@ -14,8 +15,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
 
     this.setState({ email: '', password: '' });
   };
@@ -49,11 +59,14 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <div className='buttons'>
-          <CustomButton type="submit"> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-            Sign in with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
           </div>
         </form>
       </div>
--- src/pages/signin-and-signup/signin-and-signup-component.jsx
+++ src/pages/signin-and-signup/signin-and-signup-component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/signin-component';
+import SignUp from '../../components/sign-up/signup-component';
 import './signin-and-signup-style.css';
 
 const SignInAndSignUpPage = () => (
   <div className="signin-and-signup">
     <SignIn />
+    <SignUp />
   </div>
 );
 
