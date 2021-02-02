4f2604d42634cd4aa2a2fd427112c53bb3f6e685
Lourent Flores <lourent.flores@gmail.com>
2021-02-01 21:13:08 -0500

    removed .eslintcache

--- src/components/custom-button/custom-button-component.jsx
+++ src/components/custom-button/custom-button-component.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import './custom-button-style.css';
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? 'inverted' : ''}${
-      isGoogleSignIn ? 'google-sign-in' : ''
-    } custom-button`}
-    {...otherProps}>
-    {children}
-  </button>
+import { CustomButtonContainer } from './custom-button-style';
+
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
+
+// without styled-components
+// import './custom-button-style.css';
+
+// const CustomButton = ({
+//   children,
+//   isGoogleSignIn,
+//   inverted,
+//   ...otherProps
+// }) => (
+//   <button
+//     className={`${inverted ? 'inverted' : ''}${
+//       isGoogleSignIn ? 'google-sign-in' : ''
+//     } custom-button`}
+//     {...otherProps}>
+//     {children}
+//   </button>
+// );
+
+
